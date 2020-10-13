113da5c6ebc4475977922c50a5ff52e3c6b2fa59
Spencer Summerville <me@grenuttag.com>
2020-10-13 06:26:51 -0400

    Check in WIP code

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,20 @@ import { Client, Message, MessageEmbed } from "discord.js";
 import reactions from "./reactions.js";
 
 let client: Client = new Client();
-let count: number = 0;
+
+function respond(reactions: Array<string>): MessageEmbed {
+  let randomImage: string =
+    reactions[Math.floor(Math.random() * reactions.length)];
+
+    let response: MessageEmbed = new MessageEmbed()
+    .setImage(randomImage);
+
+    return response
+}
 
 client.on("message", (message: Message) => {
   if (message.content.match(/unzip/i) !== null) {
-    count += 1;
-
-    let randomImage: string =
-      reactions[Math.floor(Math.random() * reactions.length)];
-
-    let response: MessageEmbed = new MessageEmbed()
-      .setImage(randomImage)
-      .setFooter(`${count}`);
-
+    let response: MessageEmbed = respond(reactions)
     message.reply(response);
   }
 });
